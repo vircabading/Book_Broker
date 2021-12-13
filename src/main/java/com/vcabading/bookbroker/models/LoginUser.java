1d89b7045346be5309932469e9bc8e0391f599d8
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-12 23:38:12 -0800

    add LoginUser

---
+++ src/main/java/com/vcabading/bookbroker/models/LoginUser.java
@@ -0,0 +1,85 @@
+package com.vcabading.bookbroker.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+////////////////////////////////////////////////////////////
+//	LOG IN USER CLASS
+////////////////////////////////////////////////////////////
+
+public class LoginUser {
+    
+	//	//// FIELDS ////////////////////////////////////////
+	
+    @NotEmpty(message="Email is required!")
+    @Email(message="Please enter a valid email!")
+    private String email;
+    
+    @NotEmpty(message="Password is required!")
+    @Size(min=8, max=128, message="Password must be between 8 and 128 characters")
+    private String password;
+
+	@Column(updatable=false)		// this will not allow createdAt to be updated after creation
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createdAt;
+	
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date updatedAt;
+	
+	@PrePersist
+	protected void onCreate()	{
+		this.createdAt = new Date();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+    
+	//	//// COSNTRUCTORS //////////////////////////////////
+    
+    public LoginUser() {}
+
+    //	//// GETTERS AND SETTERS ///////////////////////////
+    
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+    
+}
