b993e0605cc96fb6d9ff5313aff9596ab1cb29eb
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-12 23:17:57 -0800

    initial commit

---
+++ src/test/java/com/vcabading/bookbroker/BookbrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcabading.bookbroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookbrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
